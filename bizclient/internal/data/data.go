7781ad70b6de6056464b6db5e2b5a45f1258227e
xinghe <xinghe903@outlook.com>
2024-12-27 17:27:39 +0800

    连接server

--- bizclient/internal/data/data.go
+++ bizclient/internal/data/data.go
@@ -2,9 +2,12 @@ package data
 
 import (
 	"bizclient/internal/conf"
+	"context"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/google/wire"
+	ggrpc "google.golang.org/grpc"
 )
 
 // ProviderSet is data providers.
@@ -12,7 +15,7 @@ var ProviderSet = wire.NewSet(NewData, NewBizServerRepo)
 
 // Data .
 type Data struct {
-	// TODO wrapped database client
+	BizServer *ggrpc.ClientConn
 }
 
 // NewData .
@@ -21,4 +24,13 @@ func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
 	return &Data{}, cleanup, nil
+}
+
+func newBizServerClient() *ggrpc.ClientConn {
+	ctx := context.Background()
+	conn, err := grpc.Dial(ctx)
+	if err != nil {
+		panic("bizserver grpc dial failed" + err.Error())
+	}
+	return conn
 }
